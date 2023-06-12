d97ab6fbf94e0ebc80698c4132ad0f4212922ba5
lao king <wangjian5748@gmail.com>
2023-06-12 14:37:56 +0800

    runtime upgrade

--- lession3/pallets/kitties/src/lib.rs
+++ lession3/pallets/kitties/src/lib.rs
@@ -5,6 +5,8 @@
 /// <https://docs.substrate.io/reference/frame-pallets/>
 pub use pallet::*;
 
+mod migrations;
+
 #[cfg(test)]
 mod mock;
 
@@ -14,22 +16,30 @@ mod tests;
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::{pallet_prelude::*, Blake2_128Concat};
-	use frame_system::pallet_prelude::*;
+	use frame_system::{pallet_prelude::*};
 
-	use frame_support::traits::{Randomness, Currency, ExistenceRequirement};
+	use frame_support::traits::{Randomness, Currency, ExistenceRequirement, StorageVersion};
 	use frame_support::PalletId;
 	use sp_runtime::DispatchError;
-use sp_runtime::traits::AccountIdConversion;
+	use sp_runtime::traits::AccountIdConversion;
 
 	use sp_io::hashing::blake2_128;
+
+	use crate::migrations;
 
 	pub type KittyId = u32;
 	pub type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
 	#[derive(Encode, Decode, Clone, Copy, RuntimeDebug, PartialEq, Eq, Default, TypeInfo, MaxEncodedLen)]
-	pub struct Kitty(pub [u8; 16]);
+	pub struct Kitty {
+		pub dna: [u8; 16],
+		pub name: [u8; 4],
+	}
+
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
 
 	#[pallet::pallet]
+	#[pallet::storage_version(STORAGE_VERSION)]
 	pub struct Pallet<T>(_);
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
@@ -86,14 +96,22 @@ use sp_runtime::traits::AccountIdConversion;
 		AlreadyOwned,
 	}
 
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_runtime_upgrade() -> Weight {
+			migrations::v1::migrate::<T>()
+		}
+	}
+
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		#[pallet::call_index(0)]
 		#[pallet::weight(10_000)]
-		pub fn create(origin: OriginFor<T>) -> DispatchResult {
+		pub fn create(origin: OriginFor<T>, name: [u8; 4]) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 			let kitty_id = Self::get_next_id()?;
-			let kitty = Kitty(Self::random_value(&who));
+			let dna = Self::random_value(&who);
+			let kitty = Kitty{ dna, name };
 
 			let price = T::KittyPrice::get();
 			// T::Currency::reserve(&who, price)?;
@@ -114,7 +132,12 @@ use sp_runtime::traits::AccountIdConversion;
 
 		#[pallet::call_index(1)]
 		#[pallet::weight(10_000)]
-		pub fn breed(origin: OriginFor<T>, kitty_id_1: KittyId, kitty_id_2: KittyId) -> DispatchResult {
+		pub fn breed(
+			origin: OriginFor<T>, 
+			kitty_id_1: 
+			KittyId, kitty_id_2: KittyId, 
+			name: [u8; 4]
+		) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 
 			ensure!(kitty_id_1 != kitty_id_2, Error::<T>::SameKittyId);
@@ -131,11 +154,12 @@ use sp_runtime::traits::AccountIdConversion;
 
 			let selector = Self::random_value(&who);
 
-			let mut data = [0u8; 16];
-			for i in 0..kitty_1.0.len() {
-				data[i] = (kitty_1.0[i] & selector[i]) | (kitty_2.0[i] & !selector[i]);
-			}
-			let kitty = Kitty(data);
+			let dna = [0u8; 16];
+			// let mut data = [0u8; 16];
+			// for i in 0..kitty_1.0.len() {
+			// 	data[i] = (kitty_1.0[i] & selector[i]) | (kitty_2.0[i] & !selector[i]);
+			// }
+			let kitty = Kitty{dna, name};
 
 			let price = T::KittyPrice::get();
 			
